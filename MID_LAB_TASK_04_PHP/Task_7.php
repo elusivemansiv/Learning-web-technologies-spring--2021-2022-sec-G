c444c6c5baeb8ed1c5ccad6eb8e7694a0eb530b2
Khan,Mansib Ahsan <mansibahsan9@gmail.com>
2022-02-08 10:56:20 +0600

    php lab task

---
+++ MID_LAB_TASK_04_PHP/Task_7.php
@@ -0,0 +1,19 @@
+<?php
+    for($i=0;$i<3;$i++){
+        for($j=0;$j<=$i;$j++){
+            echo "*";
+        }
+        echo "<br/>";
+    }
+   echo "<br/>";
+
+
+   $number = array('1', '2', '3');
+    for($i=0;$i<3;$i++){
+        for($j=0;$j<=$i;$j++){
+            echo $number;
+        }
+        echo "<br/>";
+    }
+
+?>
