2d699ea5b076760f9a9f5479f4199d3f1bc313fb
Khan,Mansib  Ahsan <97165527+elusivemansiv@users.noreply.github.com>
2022-02-15 09:39:24 +0600

    Update Task_4.php

--- MID_LAB_TASK_05_PHP_FORM/Task_4.php
+++ MID_LAB_TASK_05_PHP_FORM/Task_4.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 
 $uerror = "";
@@ -44,51 +43,5 @@ if(isset($_REQUEST['submit'])){
 	</form>
     </fieldset>
 </body>
-=======
-<?php 
 
-$uerror = "";
-$gender = "";
-
-if(isset($_REQUEST['submit'])){
-		
-	if($_REQUEST['gender'] == null){
-		$uerror =  "invalid gender!";
-	}else{
-		$gender = $_REQUEST['gender'];
-	}
-}
-
-?>
-
-<html>
-<head>
-	<title>login</title>
-</head>
-<body>
-    <fieldset> <b>GENDER</b> 
-	<form method="POST" action="#">
-		<table>
-			<tr>
-
-				<td>
-                    <input type="radio" name="gender" value="<?=$gender?>"> Male
-                    <input type="radio" name="gender" value="<?=$gender?>"> Female
-                    <input type="radio" name="gender" value="<?=$gender?>"> Other
-				</td>
-				<td>
-					<?=$uerror?>
-				</td>
-			</tr>
-			<tr>
-			
-				<td>
-					<input type="submit" name="submit" value="Submit">
-				</td>
-			</tr>
-		</table>
-	</form>
-    </fieldset>
-</body>
->>>>>>> 1f8607335049628cb5dea358064143cd6285d1b5
 </html>
